4741d8357013ec7bc8d5ddadf0cdf70a608b396a
Mr. Luhwani <tolutalabi89@gmail.com>
2026-02-15 12:47:53 +0100

    Added helper methods to the utils class

--- finance-ledger/src/main/java/dev/luhwani/ledger/services/Utils.java
+++ finance-ledger/src/main/java/dev/luhwani/ledger/services/Utils.java
@@ -1,5 +1,20 @@
 package dev.luhwani.ledger.services;
 
+import java.util.regex.Pattern;
+
 public class Utils {
-    
+    public static boolean validEmail(String email) {
+        Pattern p = Pattern.compile("^[A-Za-z0-9]+@[A-za-z-]+\\.[A-Za-z]{2,}$");
+        return p.matcher(email).matches();
+    }
+
+    public static boolean validPassword(String password) {
+        Pattern p = Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[^A-Za-z0-9\\s])[^\\s]{7,20}$");
+        return p.matcher(password).matches();
+    }
+
+    public static boolean validUsername(String username) {
+        Pattern p = Pattern.compile("^[a-zA-Z0-9_-]{5,14}$");
+        return p.matcher(username).matches();
+    }
 }
