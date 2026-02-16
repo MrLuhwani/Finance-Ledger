3659dc18c0a1dd9a336726ee6cb61e98cbecebd4
Mr. Luhwani <tolutalabi89@gmail.com>
2026-02-16 23:44:10 +0100

    Added create new user methods

--- finance-ledger/src/main/java/dev/luhwani/ledger/repos/UserRepo.java
+++ finance-ledger/src/main/java/dev/luhwani/ledger/repos/UserRepo.java
@@ -1,11 +1,14 @@
 package dev.luhwani.ledger.repos;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Optional;
 
 import dev.luhwani.ledger.customExceptions.DataAccessException;
+import dev.luhwani.ledger.models.LoginData;
 
 public class UserRepo {
 
@@ -26,6 +29,31 @@ public class UserRepo {
         } catch (SQLException e) {
             throw new DataAccessException(e.getMessage(), e);
         }
+    }
 
+    public Optional<Long> registerUser(LoginData loginData) {
+        String sql = "INSERT INTO users (email, salt, username, password_hash) VALUES (?, ?, ?, ?);";
+        String email = loginData.email();
+        String salt = loginData.salt();
+        String username = loginData.username();
+        byte[] passwordHash = loginData.passwordHash();
+        try (
+                Connection conn = ConnectionManager.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);) {
+            pstmt.setString(1, email);
+            pstmt.setString(2, salt);
+            pstmt.setString(3, username);
+            pstmt.setBytes(4, passwordHash);
+            pstmt.executeUpdate();
+            try (ResultSet generatedKeys = pstmt.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    Long id = generatedKeys.getLong(1);
+                    return Optional.of(id);
+                }
+            }
+            return Optional.empty();
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage(), e);
+        }
     }
 }
--- finance-ledger/src/main/java/dev/luhwani/ledger/services/UserService.java
+++ finance-ledger/src/main/java/dev/luhwani/ledger/services/UserService.java
@@ -1,7 +1,11 @@
 package dev.luhwani.ledger.services;
 
+import java.util.Optional;
+
 import dev.luhwani.ledger.customExceptions.DataAccessException;
 import dev.luhwani.ledger.customExceptions.UIException;
+import dev.luhwani.ledger.models.LoginData;
+import dev.luhwani.ledger.models.User;
 import dev.luhwani.ledger.repos.UserRepo;
 
 public class UserService {
@@ -15,6 +19,18 @@ public class UserService {
     public boolean emailFound(String email) {
         try {
             return repo.findEmail(email);
+        } catch (DataAccessException e) {
+            throw new UIException(e.getMessage(), e);
+        }
+    }
+
+    public Optional<User> createNewAcct(LoginData loginData) {
+        try {
+            Optional<Long> userId = repo.registerUser(loginData);
+            if (userId.isPresent()) {
+                return Optional.of(new User(loginData.email(), userId.get(), loginData.username()));
+            }
+            return Optional.empty();
         } catch (DataAccessException e) {
             throw new UIException(e.getMessage(), e);
         }
