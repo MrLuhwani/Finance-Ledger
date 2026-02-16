c2dd53767c98dad8839bcdf30190be41621cd3f0
Mr. Luhwani <tolutalabi89@gmail.com>
2026-02-16 12:27:42 +0100

    Created custom exceptions

---
+++ finance-ledger/src/main/java/dev/luhwani/ledger/customExceptions/DataAccessException.java
@@ -0,0 +1,9 @@
+package dev.luhwani.ledger.customExceptions;
+
+public class DataAccessException extends RuntimeException {
+
+    public DataAccessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+}
---
+++ finance-ledger/src/main/java/dev/luhwani/ledger/customExceptions/UIException.java
@@ -0,0 +1,9 @@
+package dev.luhwani.ledger.customExceptions;
+
+public class UIException extends RuntimeException{
+    
+    public UIException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
