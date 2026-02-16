f6f800aedf3d4e9de97efec719a166f857dd561a
Mr. Luhwani <tolutalabi89@gmail.com>
2026-02-16 20:48:52 +0100

    Created method to find user email

--- finance-ledger/src/main/java/dev/luhwani/ledger/repos/UserRepo.java
+++ finance-ledger/src/main/java/dev/luhwani/ledger/repos/UserRepo.java
@@ -1,5 +1,31 @@
 package dev.luhwani.ledger.repos;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import dev.luhwani.ledger.customExceptions.DataAccessException;
+
 public class UserRepo {
-    
+
+    public boolean findEmail(String email) {
+        String sql = "SELECT email FROM users;";
+        try (
+                Connection conn = ConnectionManager.getConnection();
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql);) {
+            String result;
+            while (rs.next()) {
+                result = rs.getString("email");
+                if (result.equals(email)) {
+                    return true;
+                }
+            }
+            return false;
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage(), e);
+        }
+
+    }
 }
--- finance-ledger/src/main/java/dev/luhwani/ledger/services/UserServices.java
+++ finance-ledger/src/main/java/dev/luhwani/ledger/services/UserServices.java
@@ -1,5 +1,23 @@
 package dev.luhwani.ledger.services;
 
+import dev.luhwani.ledger.customExceptions.DataAccessException;
+import dev.luhwani.ledger.customExceptions.UIException;
+import dev.luhwani.ledger.repos.UserRepo;
+
 public class UserServices {
-    
+
+    public UserRepo repo;
+
+    public UserServices(UserRepo repo) {
+        this.repo = repo;
+    }
+
+    public boolean emailFound(String email) {
+        try {
+            return repo.findEmail(email);
+        } catch (DataAccessException e) {
+            throw new UIException(e.getMessage(), e);
+        }
+    }
+
 }
