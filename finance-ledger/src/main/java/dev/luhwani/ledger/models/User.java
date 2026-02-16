81657e770e20525519cf42d8a980c6f8c60adf56
Mr. Luhwani <tolutalabi89@gmail.com>
2026-02-16 23:40:49 +0100

    Refactored user model object class

--- finance-ledger/src/main/java/dev/luhwani/ledger/models/User.java
+++ finance-ledger/src/main/java/dev/luhwani/ledger/models/User.java
@@ -1,36 +1,23 @@
 package dev.luhwani.ledger.models;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class User {
-    
-    private String email;
+
+    private final String email;
     private final Long id;
     private String username;
-    private String password;
     private List<Transaction> transactions = new ArrayList<>();
 
-    //these two might not be actual object fields, but are here for the time-being as
-    //refernces while building
-
-    private String salt;
-    private LocalDateTime createdAt;
-
-    public User(String email, Long id, String username, String password) {
+    public User(String email, Long id, String username) {
         this.email = email;
         this.id = id;
         this.username = username;
-        this.password = password;
     }
 
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public Long getId() {
@@ -41,19 +28,7 @@ public class User {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public List<Transaction> geTransactions() {
+    public List<Transaction> getTransactions() {
         return transactions;
     }
 
