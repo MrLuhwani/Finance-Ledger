afd5e68df1974154bfbe6a719875aec57993f918
MrLuhwani <tolutalabi89@gmail.com>
2026-02-21 11:00:22 +0100

    Created methods to add new transactions

--- finance-ledger/src/main/java/dev/luhwani/ledger/repos/TransactionRepo.java
+++ finance-ledger/src/main/java/dev/luhwani/ledger/repos/TransactionRepo.java
@@ -1,8 +1,14 @@
 package dev.luhwani.ledger.repos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.EnumSet;
 
+import dev.luhwani.ledger.customExceptions.DataAccessException;
 import dev.luhwani.ledger.models.Category;
+import dev.luhwani.ledger.models.Transaction2;
 
 public class TransactionRepo {
     
@@ -10,5 +16,28 @@ public class TransactionRepo {
 
     public EnumSet<Category> allCategories(){
         return allCategories;
+    }
+
+    public void storeTransaction(Transaction2 transaction2) {
+        String sql = "INSERT INTO transactions(date, koboAmt, entry_type, category, description, user_id) VALUES (?, ?, ?, ?, ?, ?);";
+        LocalDate date = transaction2.date();
+        Long koboAmt = transaction2.koboAmt();
+        String entryType = String.valueOf(transaction2.entryType());
+        String category = String.valueOf(transaction2.category());
+        String description = transaction2.description();
+        Long userId = transaction2.userId();
+        try (
+                Connection conn = ConnectionManager.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setObject(1, date);
+            pstmt.setLong(2, koboAmt);
+            pstmt.setString(3, entryType);
+            pstmt.setString(4, category);
+            pstmt.setString(5, description);
+            pstmt.setLong(6,userId);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage(), e);
+        }
     }
 }
--- finance-ledger/src/main/java/dev/luhwani/ledger/services/TransactionService.java
+++ finance-ledger/src/main/java/dev/luhwani/ledger/services/TransactionService.java
@@ -2,7 +2,11 @@ package dev.luhwani.ledger.services;
 
 import java.util.EnumSet;
 
+import dev.luhwani.ledger.customExceptions.DataAccessException;
+import dev.luhwani.ledger.customExceptions.UIException;
 import dev.luhwani.ledger.models.Category;
+import dev.luhwani.ledger.models.Transaction2;
+import dev.luhwani.ledger.models.User;
 import dev.luhwani.ledger.repos.TransactionRepo;
 
 public class TransactionService {
@@ -15,5 +19,14 @@ public class TransactionService {
 
     public EnumSet<Category> getCategories() {
         return repo.allCategories();
+    }
+
+    public void addTransaction(User user,Transaction2 transaction2) {
+        try {
+            repo.storeTransaction(transaction2);
+            user.addTransaction(transaction2);
+        } catch (DataAccessException e) {
+            throw new UIException(e.getMessage(), e);
+        }
     }
 }
